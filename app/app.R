affe6c17c46c30db91928734721c2a9035a65d75
nrennie <nrennie35@gmail.com>
2023-10-08 19:50:37 +0100

    update app

--- app/app.R
+++ app/app.R
@@ -1,7 +1,10 @@
+# Note - the UX for package installation will likely become less
+# "hidden" over time, and will certainly become much quicker.
 webr::install("dplyr")
 webr::install("htmltools")
 webr::install("glue")
 webr::install("rlang")
+webr::install("bslib")
 
 library(dplyr)
 
@@ -13,9 +16,10 @@ all_pkgs <- colnames(all_pkgs)
 
 # Define UI
 ui <- bootstrapPage(
-  titlePanel("#TidyTuesday"),
+  theme = bslib::bs_theme(version = 5, bootswatch = "morph"),
   htmltools::div(
     class = "container-fluid",
+    titlePanel("#TidyTuesday"),
     htmltools::div(
       class = "row",
       htmltools::div(
@@ -26,31 +30,33 @@ ui <- bootstrapPage(
 
 My contributions can be found on [GitHub](https://github.com/nrennie/tidytuesday), and you can use this Shiny app to explore my visualisations with links to code for each individual plot. You can also follow my attempts on Mastodon at [fosstodon.org/@nrennie](https://fosstodon.org/@nrennie).
 "),
-        htmltools::hr(),
-        # packages
-        htmltools::tags$details(
-          htmltools::tags$summary("Filter R packages (click to expand):"),
-          shiny::radioButtons("pkg_select",
-            "Only show plots that use:",
-            choices = c("Any package", all_pkgs),
-            selected = NULL,
-            inline = TRUE
-          )
-        ),
-        # choose a plot
-        shiny::uiOutput("select_img"),
-        # display information
-        shiny::textOutput("pkgs_used"),
-        shiny::htmlOutput("code_link"),
-        shiny::htmlOutput("r4ds_link")
+htmltools::hr(),
+# packages
+htmltools::tags$details(
+  htmltools::tags$summary("Filter R packages (click to expand):"),
+  shiny::radioButtons("pkg_select",
+                      "Only show plots that use:",
+                      choices = c("Any package", all_pkgs),
+                      selected = NULL,
+                      inline = TRUE
+  )
+),
+# choose a plot
+shiny::uiOutput("select_img"),
+# display information
+shiny::textOutput("pkgs_used"),
+htmltools::br(),
+shiny::htmlOutput("code_link"),
+htmltools::br(),
+shiny::htmlOutput("r4ds_link")
       ),
-      htmltools::div(
-        class = "col-lg-6",
-        # show plot
-        htmltools::br(),
-        shiny::htmlOutput("plot_img"),
-        htmltools::br()
-      )
+htmltools::div(
+  class = "col-lg-6",
+  # show plot
+  htmltools::br(),
+  shiny::htmlOutput("plot_img"),
+  htmltools::br()
+)
     )
   )
 )
@@ -82,43 +88,43 @@ server <- function(input, output) {
     req(input$plot_title)
     dplyr::filter(all_weeks, title == input$plot_title)
   })
-
+  
   ## Image display
   img_path <- shiny::reactive({
     glue::glue("https://raw.githubusercontent.com/nrennie/tidytuesday/main/{week_data()$img_fpath}")
   })
-
+  
   output$plot_img <- shiny::renderText({
     c('<img src="', img_path(), '" width="100%">')
   })
-
+  
   ### List of packages
   output$pkgs_used <- shiny::renderText({
     glue::glue(
       "This plot uses the following packages: {week_data()$pkgs}"
     )
   })
-
+  
   ### Code link
   code_path <- shiny::reactive({
     glue::glue(
       "https://github.com/nrennie/tidytuesday/tree/main/{week_data()$code_fpath}"
     )
   })
-
+  
   output$code_link <- shiny::renderText({
     glue::glue(
       '<b>Code is available at</b>: <a href="{code_path()}"  target="_blank">{code_path()}</a>.'
     )
   })
-
+  
   ### R4DS link
   r4ds_path <- shiny::reactive({
     glue::glue(
       "https://github.com/rfordatascience/tidytuesday/blob/master/data/{week_data()$year}/{week_data()$week}/readme.md"
     )
   })
-
+  
   output$r4ds_link <- shiny::renderText({
     glue::glue('<b>R4DS GitHub link</b>: <a href="{r4ds_path()}"  target="_blank">{r4ds_path()}</a>.')
   })
